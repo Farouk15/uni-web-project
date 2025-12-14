b7b283dfb669a9be5797a69951d671a9bf4715d0
mohamed <mkshaat2006@gmail.com>
2025-12-14 20:18:20 +0200

    editing

--- backend/app.php
+++ backend/app.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'login_demo');
+define('DB_NAME', 'shoeus');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -13,4 +13,6 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+echo "Connected successfully to the database!";
+
 ?>
